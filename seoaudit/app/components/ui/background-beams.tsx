867cf6d75a8ba48b687aa28a7234099b8455aea5
harshit960 <raj.harshit960@gmail.com>
2024-05-17 13:03:08 +0530

    change

--- seoaudit/app/components/ui/background-beams.tsx
+++ seoaudit/app/components/ui/background-beams.tsx
@@ -85,7 +85,7 @@ export const BackgroundBeams = React.memo(
               d={path}
               stroke={`url(#linearGradient-${index})`}
               strokeOpacity="0.4"
-              strokeWidth="5"
+              strokeWidth="1"
             ></motion.path>
           ))}
           <defs>
--- seoaudit/app/page.tsx
+++ seoaudit/app/page.tsx
@@ -49,7 +49,7 @@ function Home() {
       <div className="text-4xl text-[#221A43] font-black text-center">
         TOOLS AVAILABLE
       </div>
-      <div className="my-10 flex flex-col items-center justify-center">
+      <div className="my-10 flex flex-col items-center justify-center overflow-hidden">
 
         <InfiniteMovingCards items={items} />
         <Button
@@ -63,7 +63,7 @@ function Home() {
     </div>
     <div className="h-[40rem] w-full rounded-md relative flex flex-col items-center justify-center ">
       <div className="max-w-2xl mx-auto p-4">
-        <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-500 to-neutral-600  text-center font-sans font-bold">
+        <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-[#221a43ce] to-[#221A43]  text-center font-sans font-bold">
         Get in touch with us
         </h1>
         <p></p>
