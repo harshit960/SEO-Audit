6987fb8b62bfa0472e19998ce3c6409df2893505
harshit960 <raj.harshit960@gmail.com>
2024-09-23 23:22:52 +0530

    mino5r change

--- seoaudit/app/tools/[test]/[index]/page.jsx
+++ seoaudit/app/tools/[test]/[index]/page.jsx
@@ -5,28 +5,10 @@ import { useEffect, useState } from 'react';
 export default function TestPage({ params }) {
   const { test } = params;  // Dynamic route parameter
   const [testData, setTestData] = useState(null);
+  const [PsiData, setPsiData] = useState();
+  const [ScrapeData, setScrapeData] = useState();
   const [loading, setLoading] = useState(false);
-
-  // Fetch data on the client side
-  useEffect(() => {
-   
-  }, [test]);
-
-  if (loading) {
-    return <div>Loading...{test}</div>;
-  }
-
-  // if (!testData) {
-  //   return <div>Test not found</div>;
-  // }
-
-  return (
-    <div>
-      {test}
-    </div>
-  );
-}
-
+  const [url, seturl] = useState('');
   const fetchData = async (url) => {
     try {
       console.log(process.env.NEXT_PUBLIC_API_HOST);
@@ -49,3 +31,28 @@ export default function TestPage({ params }) {
       console.error('Error fetching data:', error);
     }
   };
+  // Fetch data on the client side
+  useEffect(() => {
+    fetchData(test);
+  }, [test]);
+
+  if (loading) {
+    return <div>Loading...{test}</div>;
+  }
+
+  // if (!testData) {
+  //   return <div>Test not found</div>;
+  // }
+
+  return (
+    <div className='flex flex-col'>
+      {test}
+      <div className="flex">
+
+        <input type="text" onChange={(e) => seturl(e.target.value)} value={url} className='border w-96 p-4' placeholder='YourWebiste.com' />
+        <button onClick={() => fetchData(url)} className='bg-blue-500 text-white p-4'>Run Test</button>
+      </ div>
+    </div>
+  );
+}
+
